8c4d1540a3893c4e9cdac34e622ed7d34c29c2bf
root <root@localhost.localdomain>
2020-08-01 20:33:18 +0530

    adding custom user model test

--- app/app/calc.py
+++
@@ -1,3 +0,0 @@
-def add(x,y):
-    """ calculate two number"""
-    return x+y
--- app/app/test.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-from app.calc import add
-
-class CalcTest(TestCase):
-    def test_add_two_number(self):
-        """ Test that two number are add together"""
-        self.assertEqual(add(8,3),11)
