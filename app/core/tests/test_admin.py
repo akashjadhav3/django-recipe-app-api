019b03e2985410c948a25c3a01d3e7a02e79a13d
root <root@localhost.localdomain>
2020-08-02 10:40:38 +0530

    Adding postgres to docker-compose

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -26,3 +26,11 @@ class AdminSiteTest(TestCase):
         # check status code 200 success
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """Test that the user edit page works."""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        # Look like: /admin/core/user/1
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
